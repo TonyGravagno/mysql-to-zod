ec65ad15d8a8db4bce56b03b56390014c5f2a7f4
yuumillar <araera111@gmail.com>
2024-09-04 00:18:45 +0900

    feat(mergeGlobalConfig.ts): Create a function to merge the old globalConfig with the new globalConfi
    feat #156

---
+++ src/process/outputToFile/mergeGlobalConfig.ts
@@ -0,0 +1,74 @@
+import { readFileSync } from "node:fs";
+import {} from "@mobily/ts-belt";
+import { isNil } from "rambda";
+import { formatByPrettier } from "../formatByPrettier";
+type MergeGlobalConfigProps = {
+	globalSchemaPath: string;
+	newGlobalSchema: string;
+};
+type KV = {
+	key: string;
+	value: string;
+};
+
+const ignoreList = ["import", "export", "};"];
+
+const toKeyValuePair = (schemaText: string) =>
+	schemaText
+		.split("\n")
+		.filter(
+			(line) =>
+				!ignoreList.some((ignoreWord) => line.includes(ignoreWord)) &&
+				line !== "",
+		)
+		.flatMap((x) => {
+			const [key, value] = x.split(":");
+			if (!value || !key) return [];
+
+			return {
+				key: key.trim(),
+				value: value.trim(),
+			};
+		});
+
+export const mergeGlobalConfig = async ({
+	globalSchemaPath: path,
+	newGlobalSchema: globalSchema,
+}: MergeGlobalConfigProps) => {
+	const oldGlobalSchemaText = readFileSync(path, "utf-8");
+	const oldAst = toKeyValuePair(oldGlobalSchemaText);
+	const newAst = toKeyValuePair(globalSchema);
+	const res = [...oldAst, ...newAst];
+
+	const loop = (rest: KV[], keyList: string[], result: KV[]): KV[] => {
+		if (rest.length === 0) return result;
+		const [x, ...xs] = rest;
+		if (isNil(x)) return result;
+		const isExist = keyList.includes(x.key);
+		if (isExist) return loop(xs, keyList, result);
+		return loop(xs, [...keyList, x.key], [...result, x]);
+	};
+	const result = loop(res, [], []);
+	const importZod = `import { z } from "zod";\n`;
+	const exportGlobal = "export const globalSchema = {\n";
+	const body = result.map((x) => `  ${x.key}: ${x.value}`).join("\n");
+	const end = "};\n";
+	const final = await formatByPrettier(
+		`${importZod}${exportGlobal}${body}${end}`,
+	);
+	return final;
+};
+
+/* mergeGlobalConfig({
+	globalSchemaPath: "./mysqlToZod/globalSchema.ts",
+	newGlobalSchema: `import { z } from "zod";
+export const globalSchema = {
+  NNN: z.number(),
+  mysqlVARCHAR: z.string(),
+  mysqlTINYINT: z.number(),
+  mysqlTIMESTAMP: z.date(),
+};\n'
+}
+`,
+});
+ */
