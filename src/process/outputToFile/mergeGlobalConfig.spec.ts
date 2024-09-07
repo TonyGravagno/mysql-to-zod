58f13a3c16dc804a5d1da71cb99b5a07a68d226d
yuumillar <araera111@gmail.com>
2024-09-07 09:07:55 +0900

    feat(mergeGlobalConfig.spec.ts): add first test case
    feat #166

--- src/process/outputToFile/mergeGlobalConfig.spec.ts
+++ src/process/outputToFile/mergeGlobalConfig.spec.ts
@@ -1,7 +1,39 @@
+import { mergeGlobalConfig } from "./mergeGlobalConfig";
+
 describe("mergeGlobalConfig", () => {
-	it("case1", () => {
-    const old = ``;
-    const new = ``
-		expect().toBe();
+	it("case1", async () => {
+		const old = `import { z } from "zod";
+export const globalSchema = {
+  mysqlINT: z.number(),
+  mysqlVARCHAR: z.string(),
+  mysqlTINYINT: z.number(),
+  Timestamp: z.date(),
+};
+`;
+		const newFile = `import { z } from "zod";
+export const globalSchema = {
+  NNN: z.number(),
+  mysqlVARCHAR: z.string(),
+  mysqlTINYINT: z.number(),
+  mysqlTIMESTAMP: z.date(),
+};
+`;
+
+		const result = `import { z } from "zod";
+export const globalSchema = {
+  mysqlINT: z.number(),
+  mysqlVARCHAR: z.string(),
+  mysqlTINYINT: z.number(),
+  Timestamp: z.date(),
+  NNN: z.number(),
+  mysqlTIMESTAMP: z.date(),
+};
+`;
+		expect(
+			await mergeGlobalConfig({
+				oldGlobalSchema: old,
+				newGlobalSchema: newFile,
+			}),
+		).toBe(result);
 	});
 });
