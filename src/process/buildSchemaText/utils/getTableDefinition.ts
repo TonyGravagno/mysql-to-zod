5743a990cb1822f961fe610c4067800c02a80b1a
Tony Gravagno <499473+TonyGravagno@users.noreply.github.com>
2024-09-10 16:22:08 -0700

    Use new output to sql/schema functions

--- src/process/buildSchemaText/utils/getTableDefinition.ts
+++ src/process/buildSchemaText/utils/getTableDefinition.ts
@@ -1,15 +1,24 @@
 import mysql from "mysql2/promise";
 import { z } from "zod";
+import { outputSqlToFile } from "../../outputToFile";
+import { MysqlToZodOption } from "../../../options";
 
-export const getTableDefinition = async (
+export type RequestForTable = {
 	tableName: string,
-	dbConnection: string,
-) => {
-	const connection = await mysql.createConnection(dbConnection);
+	option: MysqlToZodOption
+}
+
+export const getTableDefinition = async ({ tableName, option }: RequestForTable) => {
+	// mysql2@3.11.0\node_modules\mysql2\typings\mysql\lib\Connection.d.ts
+	// biome-ignore lint/suspicious/noExplicitAny: Accepts string or ConnectionOptions
+	const connection = await mysql.createConnection(option.dbConnection as any);
 	const [table] = await connection.query("show create table ??", tableName);
 	if (!Array.isArray(table)) return [];
-	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	// biome-ignore lint/suspicious/noExplicitAny: Many possible types for mySQL table
 	const result = table.flatMap((x: any) => Object.values(x));
 	await connection.destroy();
-	return z.string().array().parse(result);
+	const sql = z.string().array().parse(result);
+	if (option?.output?.saveSql)
+		outputSqlToFile({ sql, output: option.output });
+	return sql;
 };
