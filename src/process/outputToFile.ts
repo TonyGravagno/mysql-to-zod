a64da023cab4ee15cce4199c352d4da89f8b92af
Tony Gravagno <499473+TonyGravagno@users.noreply.github.com>
2024-09-10 16:24:12 -0700

    Remove original output function/type

--- src/process/outputToFile.ts
+++ src/process/outputToFile.ts
@@ -89,39 +89,3 @@ export const outputSchemaToFile = async ({ schemaRawText, globalSchema, output }
         await writeFormattedFile(globalSchema, "babel-ts", "globalSchema.ts", output);
     }
 };
-
-type OutputParams = {
-	schemaRawText: string;
-	globalSchema: string | undefined;
-	output: OptionOutput | undefined;
-};
-
-export const outputToFile = async ({
-	schemaRawText,
-	output,
-	globalSchema,
-}: OutputParams) => {
-	const formatted = await formatByPrettier(schemaRawText);
-
-	const { fileName, outDir } = pipe(
-		output,
-		O.fromNullable,
-		O.getWithDefault({
-			fileName: "schema.ts",
-			outDir: "./mysqlToZod",
-		}),
-	);
-	mkdirpSync(outDir);
-	const savePath = join(process.cwd(), outDir, fileName);
-	writeFileSync(savePath, formatted);
-	console.info("schema file created!");
-	console.info("path: ", savePath);
-
-	/* globalSchema */
-	if (G.isNullable(globalSchema)) return;
-	const globalSchemaFormatted = await formatByPrettier(globalSchema);
-	const globalSchemaSavePath = join(process.cwd(), outDir, "globalSchema.ts");
-	writeFileSync(globalSchemaSavePath, globalSchemaFormatted);
-	console.info("\nglobalSchema file created!");
-	console.info("path: ", globalSchemaSavePath);
-};
